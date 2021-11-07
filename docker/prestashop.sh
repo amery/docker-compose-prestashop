af1d23fa5239e2e205f12e308ad236332e40ab41
Alejandro Mery <amery@jpi.io>
2021-11-07 02:05:16 +0000

    prestashop: build assets on start unless $DISABLED_MAKE = 1
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/prestashop.sh
+++ docker/prestashop.sh
@@ -11,4 +11,7 @@ info() {
 if [ "${DISABLE_MAKE:-0}" != "1" ]; then
 	info "Running composer ..."
 	run-user composer install --no-interaction
+
+	info "Build assets ..."
+	run-user ./tools/assets/build.sh
 fi

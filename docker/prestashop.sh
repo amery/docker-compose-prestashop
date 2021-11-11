9afa33fb5925d394e8baf1034580a2cae9b5d78a
Alejandro Mery <amery@jpi.io>
2021-11-11 16:59:09 +0000

    prestashop: force ssl
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- docker/prestashop.sh
+++ docker/prestashop.sh
@@ -59,7 +59,7 @@ elif [ "${PS_INSTALL_AUTO:-1}" = 1 ]; then
 	--domain="$PS_DOMAIN" --db_server=$DB_SERVER:$DB_PORT --db_name="$DB_NAME" --db_user=$DB_USER \
 	--db_password=$DB_PASSWD --prefix="$DB_PREFIX" --firstname="John" --lastname="Doe" \
 	--password=$ADMIN_PASSWD --email="$ADMIN_MAIL" --language=$PS_LANGUAGE --country=$PS_COUNTRY \
-	--all_languages=$PS_ALL_LANGUAGES --newsletter=0 --send_email=0 --ssl=$PS_ENABLE_SSL
+	--all_languages=$PS_ALL_LANGUAGES --newsletter=0 --send_email=0 --ssl=1
 fi
 
 info "Almost ! Starting web server now"
